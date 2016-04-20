8de344a363769e96ff181443df1dd9a3d864b417
dev-lluo <dev.lluo@outlook.com>
2016-04-20 20:37:17 +0800

    一点点修改

--- src/top/flyfire/reflect/ReflectiveCache.java
+++ src/top/flyfire/reflect/ReflectiveCache.java
@@ -1,7 +1,32 @@
 package top.flyfire.reflect;
 
+import java.io.ObjectStreamException;
+
 /**
  * Created by flyfire[dev.lluo@outlook.com] on 2016/4/16.
  */
-public class ReflectiveCache{
+public class ReflectiveCache {
+
+    private ReflectiveCache(){
+        if(null!=ReflectiveCacheHolder.INSTANCE){
+            throw new IllegalStateException("ReflectiveCache instance already created.");
+        }
+    }
+
+    private static class ReflectiveCacheHolder {
+        private static ReflectiveCache INSTANCE = new ReflectiveCache();
+    }
+
+    public static ReflectiveCache getInstance(){
+        return ReflectiveCacheHolder.INSTANCE;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        throw new CloneNotSupportedException();
+    }
+
+    private Object readResolve() throws ObjectStreamException {
+        return ReflectiveCacheHolder.INSTANCE;
+    }
 }
---
+++ src/top/flyfire/reflect/metainfo/TypeMetaInfo.java
@@ -0,0 +1,15 @@
+package top.flyfire.reflect.metainfo;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+/**
+ * Created by flyfire[dev.lluo@outlook.com] on 2016/4/20.
+ */
+public class TypeMetaInfo {
+
+    private Class<?> rawType;
+
+    private ParameterizedType[] parameterizedType;
+
+}
