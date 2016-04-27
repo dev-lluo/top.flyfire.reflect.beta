426f249962c635a0cbbdb25c0098e61e9aa2cfa0
dev-lluo <dev.lluo@outlook.com>
2016-04-27 20:03:01 +0800

    补充

--- src/top/flyfire/reflect/ReflectUtil.java
+++ src/top/flyfire/reflect/ReflectUtil.java
@@ -1,12 +1,14 @@
 package top.flyfire.reflect;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by flyfire[dev.lluo@outlook.com] on 2016/4/27.
  */
 public interface ReflectUtil {
     static boolean isJdkType(Class clzz){
-        return clzz.isPrimitive()||Number.class.isAssignableFrom(clzz)||Date.class.isAssignableFrom(clzz);
+        return clzz.isPrimitive()||Number.class.isAssignableFrom(clzz)||Date.class.isAssignableFrom(clzz)|| Collection.class.isAssignableFrom(clzz)|| Map.class.isAssignableFrom(clzz);
     }
 }
